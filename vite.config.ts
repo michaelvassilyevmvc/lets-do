f8e030c29f9ca0ca8b30ff4ad990478629bd5edb
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-14 14:02:39 +0500

    settings - Auto Import

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +14,22 @@ export default defineConfig({
       /* options */
     }),
     vue(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/ // .md
+      ],
+      imports: [
+        // presets
+        'vue',
+        VueRouterAutoImports,
+      ],
+      dts: true,
+      viteOptimizeDeps: true,
+    }),
     vueDevTools(),
   ],
   resolve: {
